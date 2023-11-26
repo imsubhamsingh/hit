1efab9d54840cf35879f75fc60d5d83a04689840
Subham Singh <geekysubham@gmail.com>
2023-11-27 03:19:48 +0530

    initial commit for hit

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 
 from setuptools import setup
 
-setup (name = 'hit',
-       version = '1.0.0',
-       packages = ['hit'],
-       entry_points = {
-           'console_scripts' : [
-               'hit = hit.cli:main'
-           ]
-       })
+setup(
+    name="hit",
+    version="1.0.0",
+    packages=["hit"],
+    entry_points={"console_scripts": ["hit = hit.cli:main"]},
+)
