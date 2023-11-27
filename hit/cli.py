cd867d00da8cb5bf108554652bdcae746360e120
Subham Singh <geekysubham@gmail.com>
2023-11-27 20:29:47 +0530

    add cat-file command to read hashed object

--- hit/cli.py
+++ hit/cli.py
@@ -1,5 +1,7 @@
-import argparse
 import os
+import sys
+import argparse
+
 from . import data
 
 
@@ -21,14 +23,43 @@ def parse_args():
     hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument("file")
 
+    cat_file_parser = commands.add_parser("cat-file")
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument("object")
+
     return parser.parse_args()
 
 
 def init(args):
+    """
+    This function initializes an empty hit repository in the current working directory.
+
+    Args:
+        args (argparse.Namespace): The command line arguments passed to the function.
+    """
     data.init()
     print(f"Initialized empty hit repository in {os.getcwd()}/{data.GIT_DIR}")
 
 
 def hash_object(args):
+    """
+    This function reads a file, hashes its content using SHA1, and then prints the hashed data.
+
+    Args:
+        args (argparse.Namespace): The command line arguments passed to the function.
+                                   'args.file' is expected to be the path to the file to be hashed.
+    """
     with open(args.file, "rb") as f:
         print(data.hash_object(f.read()))
+
+
+def cat_file(args):
+    """
+    This function retrieves an object from the hit repository and writes it to stdout.
+
+    Args:
+        args (argparse.Namespace): The command line arguments passed to the function.
+                                   'args.object' is expected to be the object id of the object to be retrieved.
+    """
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
--- hit/data.py
+++ hit/data.py
@@ -19,11 +19,32 @@ def hash_object(data):
     """
     This function takes in data as an argument, hashes it using SHA1,
     and then writes the hashed data into a file. The file is stored
-    in the objects directory of the GIT_DIR. The function finally
+    in the 'objects' directory of GIT_DIR. The function finally
     returns the object id (oid) of the hashed data.
+
+    Args:
+        data (bytes): The data to be hashed and written into a file.
+
+    Returns:
+        str: The object id (oid) of the hashed data.
     """
     oid = hashlib.sha1(data).hexdigest()
     with open(f"{GIT_DIR}/objects/{oid}", "wb") as out:
         out.write(data)
 
     return oid
+
+
+def get_object(oid):
+    """
+    This function opens a file in binary mode for reading.
+    The file is located in the 'objects' directory inside GIT_DIR and its name is the object id (oid).
+
+    Args:
+        oid (str): The object id of the file to be read.
+
+    Returns:
+        bytes: The content of the file as bytes.
+    """
+    with open(f"{GIT_DIR}/objects/{oid}", "rb") as f:
+        return f.read()
